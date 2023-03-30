0dad93d548d2c9ffbab641d39b53307d7afe9dbb
DELL <flora.djm5@gmail.com>
2023-03-30 10:02:57 +0800

    cp3 second commit

--- cp-1/src/main/kotlin/com/Flora/kotlin/cp3/Nomor_2.kt
+++ cp-1/src/main/kotlin/com/Flora/kotlin/cp3/Nomor_2.kt
@@ -1,30 +1,40 @@
 package com.Flora.kotlin.cp3
 
-fun main() {
-    val t = readLine()!!.toInt() // read number of test cases
-    repeat(t) { case ->
-        val n = readLine()!!.split(" ").last().toInt() // read matrix dimension
-        val matrix = Array(n) { IntArray(n) } // initialize matrix
-        for (i in 0 until n) {
-            matrix[i] = readLine()!!.split(" ").map { it.toInt() }.toIntArray() // read matrix row
-        }
-        val symmetric = isSymmetric(matrix) // check if matrix is symmetric
-        val result = if (symmetric) "Symmetric." else "Non-symmetric." // format output
-        println("Test #${case + 1}: $result")
-    }
-}
-
-fun isSymmetric(matrix: Array<IntArray>): Boolean {
+fun isSymmetric(matrix: Array<LongArray>): Boolean {
     val n = matrix.size
+    // Check that all elements are non-negative
     for (i in 0 until n) {
-        for (j in i until n) {
-            if (matrix[i][j] != matrix[j][i]) { // check if element is symmetric
+        for (j in 0 until n) {
+            if (matrix[i][j] < 0) {
+                return false
+            }
+        }
+    }
+    // Check that matrix is symmetric with respect to center
+    for (i in 0 until n) {
+        for (j in 0 until n) {
+            if (matrix[i][j] != matrix[n-i-1][n-j-1]) {
                 return false
             }
         }
     }
     return true
 }
+
+fun main() {
+    val t = readLine()!!.toInt()
+    for (testCase in 1..t) {
+        val n = readLine()!!.split(" ")[2].toInt()
+        val matrix = Array(n) { LongArray(n) }
+        for (i in 0 until n) {
+            matrix[i] = readLine()!!.split(" ").map { it.toLong() }.toLongArray()
+        }
+        val result = if (isSymmetric(matrix)) "Symmetric." else "Non-symmetric."
+        println("Test #$testCase: $result")
+    }
+}
+
+
 
 
 
